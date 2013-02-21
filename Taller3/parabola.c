ce13d2683320957a2aa5bae13971e9622615023d
ANDRES PENARANDA GARCIA <a.penaranda451@compufi10.uniandes.edu.co>
2013-02-21 14:25:32 -0500

    jueves por la tarde

--- Taller3/parabola.c
+++ Taller3/parabola.c
@@ -2,62 +2,77 @@
 #include <gsl/gsl_math.h>
 #include <gsl/gsl_eigen.h>
 
+void  *load_data(char *in, int *n_row , int *n_columns, float *mtiempo, float *pos);
+
 int main(int argc, char **argv){
 
-float *I;
+
 int i,j;
- int n_row, n_columns;
- float *data;
+int n_row, n_columns;
 
-data = load_data(argv[1], &n_row , &n_columns);
+ float *mtiempo;
+ float *pos;
 
+
+ load_data(argv[1], &n_row , &n_columns, mtiempo, pos);
+ 
+ for( i = 0; i < n_row*3; i++) {
+ 
+    printf("%f ", mtiempo[i]);
+ }
 
  return 0;
 
 }
  
-float *load_data(char *in, int *n_row , int *n_columns){
+void *load_data(char *in, int *n_row , int *n_columns, float *mtiempo, float *pos){
 
-
-  FILE *arch;
-  float *matriz;
+ FILE *arch;
+ int test;
+ float *tiempo;
  int lineas = 0;
-  
-int i,j;
+ int i,j;
+ int posi;
 
- arch = fopen(in,'r');
+ arch = fopen(in,"r");
 
  /* encuentra el numero de lineas*/
 do{
   test =  fgetc(arch);
   if(test == '\n'){
-    lineas++;}
-      i+=1;      
-  }while(test!=EOF) 
+    lineas++;}   
+ }while(test!=EOF);
 
- matriz  = malloc (lineas * 2 * sizeof(float));
+ rewind(arch);
+
+ pos  = malloc (lineas * sizeof(float));
+ tiempo = malloc (lineas * sizeof(float));
+ mtiempo = malloc (lineas *3* sizeof(float));
 
  *n_row = lineas;
  *n_columns = 2;
- 
- /* do{
-    test =  fscanf(in, "%d %d\n", pos[i], tiempo[i]) 
-      i+=1;
-        }while(test!=EOF);*/
 
 
  /* inicialisa la matriz*/
  for(i=0;i< lineas;i++){
-   /*for(j=0;j<n_columns;j++){
-      pos = j + (n_columns * i); position in the array*/
       
-      fscanf(arch, "%d %d\n", matriz[i], matriz[i+1]);
+   fscanf(arch, "%f %f\n",(float)pos[i],(float)tiempo[i]);
            
     }
-  
 
+ /*crea la matriz de los tiempos*/
+ for(i=0;i< lineas;i++){
+   for(j=0;j<3;j++){
+     posi = j + (3 * i);
+	if(j==0){
+	  mtiempo[posi]=1;}
+        if(j==1){
+	  mtiempo[posi]=tiempo[i];}
+	if(j==2){
+	  mtiempo[posi]=(1/2)*(tiempo[i])*(tiempo[i]);}
+   }  
+ }
 
- fclose(in);
- return matriz;
+ fclose(arch);
 
 }
